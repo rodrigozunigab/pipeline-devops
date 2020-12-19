2f56a6e32fb9a94d934cec8871c4faa88513d903
rodrigozunigab <rodrigozunigab@gmail.com>
2020-12-19 12:44:49 -0300

    commit inicial pipeline remoto

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -1,46 +1,46 @@
 def call(stageOptions){
   
-        env.EJCUTARBUILD = false;
+        def ejecutarBuild = false;
         stage("Build & Test"){   
             env.TAREA =  env.STAGE_NAME 
-            env.EJCUTARBUILD =false;
+            ejecutarBuild =false;
 
             if (stageOptions.contains('Build') || (stageOptions ==''))  {   
                 sh "./gradlew clean build -x test" 
-                env.EJCUTARBUILD =true;
+                buildEjecutado =true;
             } 
-            if ((stageOptions.contains('Test') || (stageOptions =='')) && (env.EJCUTARBUILD) )     
+            if ((stageOptions.contains('Test') || (stageOptions =='')) && (buildEjecutado) )     
                 sh "./gradlew clean build"   
                 //    sh "./gradlew clean build -x build"        
         }
         stage("Sonar"){
             env.TAREA =  env.STAGE_NAME 
-            if (!env.EJCUTARBUILD) {
+            if (!buildEjecutado) {
                 currentBuild.result = 'FAILURE'
                 echo "No se puede ejecutar Sonar sin haber ejecutado un Build"
             }    
 
             def scannerHome = tool 'sonar-scanner';    
             withSonarQubeEnv('sonar-server') { 
-                if ((stageOptions.contains('Sonar') || (stageOptions =='')) && (env.EJCUTARBUILD) )
+                if ((stageOptions.contains('Sonar') || (stageOptions =='')) && (buildEjecutado) )
                     sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"   
             }                        
         }
         stage("Run"){
             env.TAREA =  env.STAGE_NAME 
-            if ((stageOptions.contains('Run') || (stageOptions =='')) && (env.EJCUTARBUILD) ){ 
+            if ((stageOptions.contains('Run') || (stageOptions =='')) && (buildEjecutado) ){ 
                 sh "nohup bash gradlew bootRun &"
                 sleep 20 
             }                          
         }
         stage("Rest"){
             env.TAREA =  env.STAGE_NAME 
-            if ((stageOptions.contains('Rest') || (stageOptions =='')) && (env.EJCUTARBUILD) ) 
+            if ((stageOptions.contains('Rest') || (stageOptions =='')) && (buildEjecutado) ) 
                 sh 'curl -X GET "http://localhost:8081/rest/mscovid/test?msg=testing"'
         }  
         stage("Nexus"){    
             env.TAREA =  env.STAGE_NAME   
-            if ((stageOptions.contains('Nexus') || (stageOptions =='')) && (env.EJCUTARBUILD) )      
+            if ((stageOptions.contains('Nexus') || (stageOptions =='')) && (buildEjecutado) )      
                 nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '2.0.1']]]                     
         }  
              
