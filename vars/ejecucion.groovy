5724711095b422f4dfdecb04d1ebcf87fa00f0ae
rodrigozunigab <rodrigozunigab@gmail.com>
2020-12-28 22:23:22 -0300

    commit inicial repo pipeline

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -12,10 +12,10 @@ pipeline {
                 script {
                 //segun el valor del parametro se debe llamar a gradle o maven
                 env.TAREA = ''
-                echo "HERRAMIENTA SELECCIONADA: ${params.HERRAMIENTA}" 
-                echo "PARAMETROS SELECCIONADOS: ${stage}"   
-                echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL}"   
-                echo "Rama ${env.BRANCH_NAME}" 
+                echo "1.-HERRAMIENTA SELECCIONADA: ${params.HERRAMIENTA}" 
+                echo "2.-PARAMETROS SELECCIONADOS: ${stage}"   
+                echo "3.-Running ${env.BUILD_ID} on ${env.JENKINS_URL}"   
+                echo "4.-Rama ${env.BRANCH_NAME}" 
 
                                           
                 if (params.HERRAMIENTA == 'gradle'){
