638055c2d9ec135a5d9108039f4a2530397b05d7
rodrigozunigab <rodrigozunigab@gmail.com>
2021-01-05 11:43:10 -0300

    commit inicial pipeline remoto

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -1,6 +1,7 @@
 def call(stageOptions){
   
        def ejecutarBuild = false;
+       
         stage("Build & Test"){   
             env.TAREA =  env.STAGE_NAME 
             ejecutarBuild =false;
@@ -28,17 +29,20 @@ def call(stageOptions){
                     sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"   
             }                        
         }
+
         stage("Run"){
             env.TAREA =  env.STAGE_NAME 
             if ((stageOptions.contains('Run') || (stageOptions =='')) && (buildEjecutado) ){ 
                 sh "nohup bash gradlew bootRun &"
                 sleep 20                        
+            }
         }
         stage("Rest"){
             env.TAREA =  env.STAGE_NAME 
             if ((stageOptions.contains('Rest') || (stageOptions =='')) && (buildEjecutado) ) 
                 sh 'curl -X GET "http://localhost:8081/rest/mscovid/test?msg=testing"'
         }  
+
         stage("Nexus"){    
             env.TAREA =  env.STAGE_NAME   
             if ((stageOptions.contains('Nexus') || (stageOptions =='')) && (buildEjecutado) )          
