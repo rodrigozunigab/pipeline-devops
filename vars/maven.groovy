908167b6cbe7f0e2f3969bcb45d79958670d8763
rodrigozunigab <rodrigozunigab@gmail.com>
2021-01-05 12:08:37 -0300

    commit inicial pipeline remoto

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,26 +1,53 @@
 def call(stageOptions){
+        def buildEjecutado = false;
   
-          stage('Compile Code') {
+        stage('Compile Code') {
                 env.TAREA =  env.STAGE_NAME 
-                sh 'mvn clean compile -e'
+                buildEjecutado =false;
+                if (stageOptions.contains('Compile Code') || (stageOptions ==''))  { 
+                    sh 'mvn clean compile -e'
+                    
+                }
         }
+
         stage('Test Code') {     
-                env.TAREA =  env.STAGE_NAME       
-                sh 'mvn clean test -e'
+                env.TAREA =  env.STAGE_NAME  
+                if ((stageOptions.contains('Test') || (stageOptions =='')) ) {      
+                    sh 'mvn clean test -e'
+                } 
         }
         stage('Jar ') {        
-                env.TAREA =  env.STAGE_NAME      
-                sh 'mvn clean package -e' 
+                env.TAREA =  env.STAGE_NAME 
+                if ((stageOptions.contains('Test') || (stageOptions ==''))) {      
+                    sh 'mvn clean package -e' 
+                    buildEjecutado =true;
+                }
         }
         stage('SonarQube analysis') {
-                env.TAREA =  env.STAGE_NAME 
-                withSonarQubeEnv(installationName: 'sonar-server') { 
-                sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
-          }
+            env.TAREA =  env.STAGE_NAME 
+            if (!buildEjecutado) {
+                currentBuild.result = 'FAILURE'
+                echo "No se puede ejecutar Sonar sin haber ejecutado un Build"
+                buildEjecutado = false;
+            }    
+
+            def scannerHome = tool 'sonar-scanner';    
+            withSonarQubeEnv('sonar-server') { 
+                if ((stageOptions.contains('Sonar') || (stageOptions =='')) && (buildEjecutado) )
+                    sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"   
+            }  
         }
+
+        stage("Rest"){
+            env.TAREA =  env.STAGE_NAME 
+            if ((stageOptions.contains('Rest') || (stageOptions =='')) && (buildEjecutado) ) 
+                sh 'curl -X GET "http://localhost:8081/rest/mscovid/test?msg=testing"'
+        }          
+
         stage('Upload Nexus') {  
-                env.TAREA =  env.STAGE_NAME           
-                nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: '/Users/servidorcasa/Documents/Cursos/2020_devops/ejemplo_maven_24_11_2020/ejemplo-maven/build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '3.0.1']]]
+            env.TAREA =  env.STAGE_NAME   
+            if ((stageOptions.contains('Nexus') || (stageOptions =='')) && (buildEjecutado) )          
+                nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '2.0.1']]]                     
         }            
 
 }
