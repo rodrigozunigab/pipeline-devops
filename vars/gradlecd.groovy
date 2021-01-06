9997af92afad7241f8748d208ea36c5ed7527b14
rodrigozunigab <rodrigozunigab@gmail.com>
2021-01-05 22:45:15 -0300

    cicd

--- vars/gradlecd.groovy
+++ vars/gradlecd.groovy
@@ -1,31 +1,34 @@
 def call(){
   
-        def buildEjecutado = false;
+        def downloadOK = false;
         echo 'Gradle'
         echo 'Despliegue Continuo'
 
         stage("downloadNexus"){    
-            env.TAREA =  env.STAGE_NAME   
-            if (buildEjecutado)          
-                nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '2.0.1']]]                     
-        }  
+            env.TAREA =  env.STAGE_NAME       
+            sh 'curl -X GET -u admin:admin http://localhost:8081/repository/test-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O' 
+            downloadOK = true;
+        }
+
 
         stage("runDownloadedJar"){    
             env.TAREA =  env.STAGE_NAME   
-            if (buildEjecutado)          
-                nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '2.0.1']]]                     
+            if (downloadOK) {
+                sh "nohup bash gradlew bootRun &"
+                sleep 20   
+            }             
         }  
 
         stage("rest"){
             env.TAREA =  env.STAGE_NAME 
-            if (buildEjecutado) 
+            if (downloadOK) 
                 sh 'curl -X GET "http://localhost:8081/rest/mscovid/test?msg=testing"'
         }  
 
         stage("nexusCD"){    
             env.TAREA =  env.STAGE_NAME   
-            if (buildEjecutado)          
-                nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '2.0.1']]]                     
+            if (downloadOK)          
+                nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: 'release-v1.0.0']]]                     
         }                    
 
 }
